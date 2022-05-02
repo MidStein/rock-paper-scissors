c09da483d2a3ddbb1997b0a33da2837e8522d4bf
MidStein <deepakchauhan19feb@gmail.com>
2022-05-02 11:12:50 +0530

    finished game for console

--- play.js
+++ play.js
@@ -10,15 +10,34 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
-        return 'Draw!!!'
+        console.log(`Draw! Both of you chose ${capitalize(playerSelection)}`)
+        return 0
     } else if ((playerSelection === 'rock' && computerSelection === 'paper') 
     || (playerSelection === 'paper' && computerSelection === 'scissors')
-    || (playerSelection === 'Scissors' && computerSelection === 'rock')) {
-        return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
+    || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        console.log(`You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`)
+        return -1
     } else {
-        return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerS)}`
+        console.log(`You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`)
+        return 1
     }
 }
 function capitalize(str) {
     return str[0].toUpperCase() + str.substring(1, str.length)
 }
+function game() {
+    let playerScore = 0
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Make your choice')
+        let computerSelection = computerPlay()
+        playerScore += playRound(playerSelection, computerSelection)
+    }
+    if (playerScore > 0) {
+        console.log('You have won the game')
+    } else if (playerScore === 0) {
+        console.log('The game resulted in a draw')
+    } else {
+        console.log('The computer has won the game')
+    }
+}
+game()
